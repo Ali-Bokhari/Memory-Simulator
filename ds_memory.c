7a75b7cf28d3a1af68e1f45da2fb56b325f84d40
Ali Bokhari <abokhari@uoguelph.ca>
2019-09-25 15:12:31 -0400

    Almost finished memory manager

--- ds_memory.c
+++ ds_memory.c
@@ -66,6 +66,37 @@ long ds_malloc( long amount ){
   return -1;
 }
 
+void ds_free( long start ){
+  int i;
+  for(i = 0; i < MAX_BLOCKS; i++){
+    if (ds_file.block[i].start == start){
+      ds_file.block[i].alloced = 0;
+    }
+  }
+}
+
+void *ds_read( void *ptr, long start, long bytes){
+  if (fseek(ds_file.fp, (sizeof(struct ds_blocks_struct)*MAX_BLOCKS) + start, SEEK_SET) != 0){
+    return NULL;
+  }
+  if (fread(ptr, sizeof(char), bytes, ds_file.fp) != bytes){
+    return NULL;
+  }
+  ds_counts.reads += 1;
+  return ptr;
+}
+
+long ds_write( long start, void *ptr, long bytes){
+  if (fseek(ds_file.fp, (sizeof(struct ds_blocks_struct)*MAX_BLOCKS) + start, SEEK_SET) != 0){
+    return -1;
+  }
+  if (fwrite(ptr, sizeof(char), bytes, ds_file.fp) != bytes){
+    return -1;
+  }
+  ds_counts.writes += 1;
+  return start;
+}
+
 int main(){
   return 0;
 }
