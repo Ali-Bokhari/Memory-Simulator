823b949afa03dd0e867d89f022229be46d1d056b
Ali Bokhari <abokhari@uoguelph.ca>
2019-09-27 18:42:45 -0400

    Finished linked list manager

--- ds_list.c
+++ ds_list.c
@@ -77,6 +77,98 @@ int ds_insert(int value, long index){
   return 0;
 }
 
+int ds_delete(long index){
+  struct ds_list_item_struct previous, new;
+  long previous_loc = 0;
+  int i;
+  if (ds_read(&previous.next, 0, sizeof(long)) == NULL){
+    return 1;
+  }
+  for (i = index; i > 0; i--){
+    if(previous.next == -1){
+      return -1;
+    }
+    previous_loc = previous.next;
+    if (ds_read(&previous, previous.next, sizeof(struct ds_list_item_struct)) == NULL){
+      return 1;
+    }
+  }
+  if (ds_read(&new, previous.next, sizeof(struct ds_list_item_struct)) == NULL){
+    return 1;
+  }
+  ds_free(previous.next);
+  previous.next = new.next;
+  if(previous_loc == 0){
+    if (ds_write(previous_loc, &previous.next, sizeof(long)) == -1){
+      return 2;
+    }
+  } else {
+    if (ds_write(previous_loc, &previous, sizeof(struct ds_list_item_struct)) == -1){
+      return 2;
+    }
+  }
+  return 0;
+}
+
+int ds_swap(long index1, long index2){
+  int i,j,temp;
+  long addy1, addy2, prev;
+  struct ds_list_item_struct struct1, struct2;
+  struct ds_list_item_struct list;
+  if (ds_read(&list.next, 0, sizeof(long)) == NULL){
+    return 1;
+  }
+  for (i = 0; list.next != -1; i++){
+    prev = list.next;
+    if (ds_read(&list, list.next, sizeof(struct ds_list_item_struct)) == NULL){
+      return 1;
+    }
+    if(index1 == i){
+      printf("index one found\n");
+      addy1 = prev;
+      struct1 = list;
+      i++;
+      for(j = i; list.next != -1; j++){
+        prev = list.next;
+        if (ds_read(&list, list.next, sizeof(struct ds_list_item_struct)) == NULL){
+          return 1;
+        }
+        if (index2 == j){
+          printf("index2 found\n");
+          addy2 = prev;
+          struct2 = list;
+          temp = struct2.item;
+          struct2.item = struct1.item;
+          struct1.item = temp;
+          ds_write(addy1, &struct1, sizeof(struct ds_list_item_struct));
+          ds_write(addy2, &struct2, sizeof(struct ds_list_item_struct));
+          return 0;
+        }
+      }
+    } else if (index2 == i){
+      addy2 = prev;
+      struct2 = list;
+      i++;
+      for(j = i; list.next != -1; j++){
+        prev = list.next;
+        if (ds_read(&list, list.next, sizeof(struct ds_list_item_struct)) == NULL){
+          return 1;
+        }
+        if (index1 == j){
+          addy1 = prev;
+          struct1 = list;
+          temp = struct1.item;
+          struct1.item = struct2.item;
+          struct2.item = temp;
+          ds_write(addy1, &struct1, sizeof(struct ds_list_item_struct));
+          ds_write(addy2, &struct2, sizeof(struct ds_list_item_struct));
+          return 0;
+        }
+    }
+  }
+} return 1;
+}
+
 int ds_find(int target){
   struct ds_list_item_struct list;
   int i;
@@ -106,6 +198,13 @@ int ds_read_elements(char * filename){
     ds_insert(value, i);
   }
   return 2;
+}
+
+int ds_finish_list(){
+  if (ds_finish()==0){
+    return 1;
+  }
+  return 0;
 }
 
 void print_list(){
